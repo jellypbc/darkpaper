da591e0fd491ca7a6b027ff44d3f940efa6158ae
cindy <ciindy.wu@gmail.com>
2020-12-21 15:06:33 -1000

    Updates .css file to .scss, centers and glitches hero

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
-import '../css/app.css'
+import '../css/app.scss'
 
 function App() {
-  return <h1>The future is dark_</h1>
+  return (
+    <div id="app">
+      <div id="wrapper">
+        <h1 className="glitch" data-text="The future is dark_">The future is dark_</h1>
+      </div>
+    </div>
+  ) 
 }
+
 
 export default App
